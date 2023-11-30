ef5b9143b97bdcedd5f65f88525adfdda50f4eaf
mehdi-mtir <mehdi.mtir@gmail.com>
2023-11-30 11:04:01 +0100

    edit with service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ListBookComponent } from './list-book/list-book.component';
 import { AddBookComponent } from './add-book/add-book.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
 
 const routes: Routes = [
   {path : '', component : HomeComponent },
   {path : 'books', component : ListBookComponent},
-  {path : 'books/add', component : AddBookComponent}
+  {path : 'books/add', component : AddBookComponent},
+  {path : 'books/edit/:id', component : EditBookComponent}
 ];
 
 @NgModule({
--- src/app/edit-book/edit-book.component.ts
+++ src/app/edit-book/edit-book.component.ts
@@ -1,21 +1,26 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
 import { Book } from '../model/book';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-edit-book',
   templateUrl: './edit-book.component.html',
   styleUrls: ['./edit-book.component.css']
 })
-export class EditBookComponent implements OnInit, OnChanges {
-  @Input() book! : Book;
-  @Output() bookEdited = new EventEmitter<Book>();
-  editForm = new FormGroup({
+export class EditBookComponent implements OnInit  {
+  book! : Book;
+   editForm = new FormGroup({
     title : new FormControl('',Validators.required),
     author : new FormControl('', Validators.required),
     price : new FormControl('')
   })
 
+  constructor(
+    private activatedRoute : ActivatedRoute,
+    private bookService : BookService,
+    private router : Router){}
 
   editBook(){
     const editedBook = new Book(
@@ -23,22 +28,21 @@ export class EditBookComponent implements OnInit, OnChanges {
       this.editForm.value.title || '',
       this.editForm.value.author || '',
       this.editForm.value.price?+this.editForm.value.price:0);
-    this.bookEdited.emit(editedBook);
+    this.bookService.editBook(editedBook);
+    this.router.navigate(['/books']);
   }
 
-  ngOnChanges(): void {
-    this.editForm.setValue({
-      title : this.book.title,
-      author : this.book.author,
-      price : this.book.price+''
-    })
-}
-
   ngOnInit(): void {
-      this.editForm.setValue({
-        title : this.book.title,
-        author : this.book.author,
-        price : this.book.price+''
-      })
+    this.activatedRoute.params.subscribe(
+      params=>{
+        this.book = this.bookService.getBookById(+params['id']);
+        console.log(this.book);
+        this.editForm.setValue({
+          title : this.book.title,
+          author : this.book.author,
+          price : this.book.price+''
+        })
+      }
+    )
   }
 }
